3d931bb7a9d7db019accc3c863ba7f390e94df4c
RAXAAXXAXAXAXAXXAXAXAXAXXAXAXAXAXLLLLLLLLLIÓÒÓÒÓÒÓÒ´´O``´`´`´´´´`´Ò´`´`´`O <ramxaxlon@gmail.com>
2017-08-04 20:19:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-require('http').createServer((req,res)=>{res.end("Hello world!")}).listen('80')
+require('http').createServer(function(req,res){res.end("Hello world!")}).listen('80',function(){console.log('Server listening.')})
